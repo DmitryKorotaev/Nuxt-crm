52abbb555178da47ab0c2fd1a958b99cc593cb5f
DmitryKorotaev <dm.kor98@mail.ru>
2025-01-26 20:02:52 +0300

    уствновка пакетов для css

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Roboto: true,
+          Inter: [400, 700],
+          "Josefin+Sans": true,
+          Lato: [100, 300],
+          Raleway: {
+            wght: [100, 400],
+            ital: [100],
+          },
+          "Crimson Pro": {
+            wght: "200..900",
+            ital: "200..700",
+          },
+        },
+      },
+    ],
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "Ui",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
