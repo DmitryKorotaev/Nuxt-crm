c020c9d0e55e62135ab801d5f26f5048eb64d1d5
DmitryKorotaev <dm.kor98@mail.ru>
2025-01-29 23:03:30 +0300

    Создание базы данных

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2x1 md-10">CRM</h1>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2x1 font-bold text-center md-5">login</h1>
+      <form></form>
+    </div>
+  </div>
+</template>
